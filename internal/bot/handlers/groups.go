c6bb9413aa2bb4e9894d6d70e866f27326808eff
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-12-02 18:14:16 +0700

    [groups] save group admin ID

--- internal/bot/handlers/groups.go
+++ internal/bot/handlers/groups.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/capcom6/lucky-pick-tg-bot/internal/groups"
+	"github.com/capcom6/lucky-pick-tg-bot/internal/users"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"go.uber.org/zap"
@@ -12,16 +13,18 @@ import (
 type Groups struct {
 	BaseHandler
 
+	usersSvc  *users.Service
 	groupsSvc *groups.Service
 }
 
-func NewGroups(bot *bot.Bot, groupsSvc *groups.Service, logger *zap.Logger) Handler {
+func NewGroups(bot *bot.Bot, usersSvc *users.Service, groupsSvc *groups.Service, logger *zap.Logger) Handler {
 	return &Groups{
 		BaseHandler: BaseHandler{
 			bot:    bot,
 			logger: logger,
 		},
 
+		usersSvc:  usersSvc,
 		groupsSvc: groupsSvc,
 	}
 }
@@ -45,17 +48,32 @@ func (h *Groups) handle(ctx context.Context, _ *bot.Bot, update *models.Update) 
 		return
 	}
 
+	user, err := h.usersSvc.RegisterUser(ctx, users.UserIn{
+		TelegramUserID: update.MyChatMember.From.ID,
+		Username:       update.MyChatMember.From.Username,
+		FirstName:      update.MyChatMember.From.FirstName,
+		LastName:       update.MyChatMember.From.LastName,
+	})
+	if err != nil {
+		h.logger.Error("failed to register user", zap.Error(err))
+		return
+	}
+
 	switch update.MyChatMember.NewChatMember.Type {
 	case models.ChatMemberTypeOwner, models.ChatMemberTypeAdministrator:
-		if err := h.groupsSvc.CreateOrUpdate(ctx, update.MyChatMember.Chat.ID, update.MyChatMember.Chat.Title); err != nil {
-			h.logger.Error("failed to create or update group", zap.Error(err))
+		if createErr := h.groupsSvc.CreateOrUpdate(
+			ctx,
+			groups.Group{TelegramID: update.MyChatMember.Chat.ID, Title: update.MyChatMember.Chat.Title},
+			groups.Admin{UserID: user.ID},
+		); createErr != nil {
+			h.logger.Error("failed to create or update group", zap.Error(createErr))
 		}
 	case models.ChatMemberTypeMember,
 		models.ChatMemberTypeRestricted,
 		models.ChatMemberTypeLeft,
 		models.ChatMemberTypeBanned:
-		if err := h.groupsSvc.Disable(ctx, update.MyChatMember.Chat.ID); err != nil {
-			h.logger.Error("failed to disable group", zap.Error(err))
+		if disableErr := h.groupsSvc.Disable(ctx, update.MyChatMember.Chat.ID); disableErr != nil {
+			h.logger.Error("failed to disable group", zap.Error(disableErr))
 		}
 	default:
 		h.logger.Warn("unknown chat member type", zap.String("type", string(update.MyChatMember.NewChatMember.Type)))
