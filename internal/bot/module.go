980796ad7aa15072572b89d3e90a34feb3d2c167
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-01-09 07:22:43 +0700

    [bot] add commands registration

--- internal/bot/module.go
+++ internal/bot/module.go
@@ -1,37 +1,52 @@
 package bot
 
 import (
+	"context"
+
 	"github.com/capcom6/lucky-pick-tg-bot/internal/bot/handlers"
 	"github.com/capcom6/lucky-pick-tg-bot/internal/bot/middlewares/callback"
 	"github.com/capcom6/lucky-pick-tg-bot/internal/bot/middlewares/state"
 	"github.com/capcom6/lucky-pick-tg-bot/internal/bot/middlewares/user"
+	"github.com/capcom6/lucky-pick-tg-bot/internal/fsm"
+	"github.com/capcom6/lucky-pick-tg-bot/internal/users"
 	"github.com/go-core-fx/logger"
 	"github.com/go-telegram/bot"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 func Module() fx.Option {
 	return fx.Module(
 		"bot",
 		logger.WithNamedLogger("bot"),
-		fx.Provide(fx.Annotate(callback.NewMiddleware, fx.ResultTags(`group:"middlewares"`))),
-		fx.Provide(fx.Annotate(state.NewMiddleware, fx.ResultTags(`group:"middlewares"`))),
-		fx.Provide(fx.Annotate(user.NewMiddleware, fx.ResultTags(`group:"middlewares"`))),
 		fx.Provide(
-			fx.Annotate(
-				func(mws []bot.Middleware) []bot.Option {
-					return []bot.Option{
-						bot.WithAllowedUpdates(bot.AllowedUpdates{
-							"message",
-							"callback_query",
-							"my_chat_member",
-						}),
-						bot.WithMiddlewares(mws...),
-					}
-				},
-				fx.ParamTags(`group:"middlewares"`),
-			),
+			func(usersSvc *users.Service, stateSvc *fsm.Service, log *zap.Logger) []bot.Option {
+				return []bot.Option{
+					bot.WithAllowedUpdates(bot.AllowedUpdates{
+						"message",
+						"callback_query",
+						"my_chat_member",
+					}),
+					bot.WithMiddlewares(
+						user.NewMiddleware(usersSvc, log),
+						state.NewMiddleware(stateSvc, log),
+						callback.NewMiddleware(log),
+					),
+				}
+			},
 		),
 		handlers.Module(),
+		fx.Invoke(func(lc fx.Lifecycle, b *bot.Bot, log *zap.Logger) {
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					RegisterCommands(ctx, b, log)
+
+					return nil
+				},
+				OnStop: func(context.Context) error {
+					return nil
+				},
+			})
+		}),
 	)
 }
